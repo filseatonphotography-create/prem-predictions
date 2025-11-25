e49f0b585b1c7a10a236135b0864adc85353d2d6
Philip <pse2@VNMB-PE-05.local>
2025-11-25 22:34:01 +0700

    Fix VS vertical alignment using alignSelf and lineHeight

--- src/App.js
+++ src/App.js
@@ -1957,7 +1957,7 @@ const leaderboard = useMemo(() => {
                             fontWeight: 700,
                             alignSelf: "center",
                             lineHeight: "32px",
-                            marginTop: 3,  
+                            marginTop: 4,  
                           }}
                         >
                           VS
