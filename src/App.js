4092d92b6857ac7781f8f2c857995cb09acdce2e
Philip <pse2@VNMB-PE-05.local>
2025-11-26 22:07:56 +0700

    Force rebuild with clean TEAM_BADGES

--- src/App.js
+++ src/App.js
@@ -85,16 +85,7 @@ const TEAM_BADGES = {
   Arsenal: "/badges/arsenal.png",
   Liverpool: "/badges/liverpool.png",
   // Add more teams later as you add files
-};
-
-// Optional team badges – flat icons to show above team codes.
-// These paths assume you'll put PNG files in public/badges, e.g. public/badges/arsenal.png
-const TEAM_BADGES = {
-  Arsenal: "/badges/arsenal.png",
-  Liverpool: "/badges/liverpool.png",
-  // Add more teams later as you add files:
-  // "Manchester City": "/badges/man_city.png",
-  // "Tottenham Hotspur": "/badges/spurs.png",
+  
 };
 
 // Spreadsheet weekly totals (historic seed)
